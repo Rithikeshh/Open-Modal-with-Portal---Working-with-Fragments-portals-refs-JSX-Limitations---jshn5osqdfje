2f10896df191e64292b2c6a1df53aaf579334987
Rithikeshh <rithikeshh@gmail.com>
2023-09-28 01:31:03 +0530

    updated

--- components/Modal.js
+++ components/Modal.js
@@ -1,17 +1,19 @@
-
-import ReactDOM from 'react-dom';
+'use client'
+import ReactDOM, { createPortal } from 'react-dom';
 
 const Modal = ({ children, isOpen, onClose }) => {
   if (!isOpen) return null;
 
  //add code here
-    <div className='modal'>
-      
+ return(
+    createPortal(<div className='modal'>
+      {children}
       <div style={{ backgroundColor: 'white', padding: '2rem', position: 'relative' }} onClick={(e) => e.stopPropagation()} >// This prevents the modal from closing when clicking inside
    //add code here
       </div>
     </div>,
-    document.body
+    document.body)
+ )
 
 };
 
